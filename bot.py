f086aee7356e013423c5cd4ea5a95b8491ee8db5
kennyuser18-tech <kenny.user18@gmail.com>
2026-09-06 00:24:26 +0500

    yangi komiks

--- bot.py
+++ bot.py
@@ -49,6 +49,10 @@ KOMIKSLAR = {
 "4-qism": "BQACAgIAAxkBAAIewmqP-7DxaRQU53BYMYzMyWNCe_HfAAINqAACGEmBSG_tNBgacM5nPQQ",
 "5-qism (Final)": "BQACAgIAAxkBAAIf0WqSusAPPB1aCVB-gKqyb9qsOvEoAALqlAACsdyYSLq_1B34Qs_wPQQ",
     },
+"OY RITSARI: qORA, OQ & QON": {
+        "1-qism": "BQACAgIAAxkBAAJBAAFqnGwNJfjy99t1na56SxDYXv8kxwACIbEAAnUH4UjbZmzgxfFX_j0E",
+    },
+
 
 }
 
